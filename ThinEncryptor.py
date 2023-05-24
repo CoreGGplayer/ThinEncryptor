6e06effee743c4732f5478ffe7c36a014a755466
CoreGGplayer <106071679+CoreGGplayer@users.noreply.github.com>
2023-05-24 18:53:20 +0800

    Update ThinEncryptor.py

--- ThinEncryptor.py
+++ ThinEncryptor.py
@@ -5,12 +5,12 @@ for i in byte1:
     temporary_var_1=int(i)
     byte2+=str(temporary_var_1)
 for i in range(len(byte2)):
-    for i in byte2:
+    for j in byte2:
         random_num_1=random.randint(0,len(byte2))
-        for i in range(random_num_1):
+        for k in range(random_num_1):
             random_num_2=random.randint(0,25)
             byte3+=string_list[random_num_2]
-        byte3+=str(i)
+        byte3+=str(j)
     result_list.append(byte3)
     byte3=""
 for i in result_list:
